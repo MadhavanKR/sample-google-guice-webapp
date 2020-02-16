786300fed8434e7706782348220dc7e3ff1d894d
MadhavanKR <madhavan.kalkunte@gmail.com>
2020-02-16 21:20:15 +0530

    Added an authentication filter. Authenticates the user based on his fb bearer token. if token is invalid, he will be denied to access the resources.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.google.inject.servlet.GuiceFilter;
 import configuration.AppServletContextListener;
+import filters.FbAuthenticationFilter;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.DefaultServlet;
 import org.eclipse.jetty.servlet.ServletContextHandler;
@@ -11,6 +12,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Server server = new Server(8080);
         ServletContextHandler servletContextHandler = new ServletContextHandler();
+        servletContextHandler.addFilter(FbAuthenticationFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
         servletContextHandler.addFilter(GuiceFilter.class, "/*", EnumSet.allOf(DispatcherType.class));
         servletContextHandler.addEventListener(new AppServletContextListener());
         server.setHandler(servletContextHandler);
